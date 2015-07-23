57fe9bafc50c2ee68ea8f063cfbf38a7c12efde7
tsheasha <tarek.sheasha@gmail.com>
2015-07-23 13:01:53 +0200

    Adding more tests for the sanity checks

--- tests/test_search.py
+++ tests/test_search.py
@@ -79,5 +79,55 @@ class TestProximitySearch(object):
             'tags': 'lights'
         }
         resp = requests.get(url, payload)
-        print resp.json()
         assert len(resp.json()['products']) == 10
+
+        payload = {
+            'lat': '59.33265972650577',
+            'lng': '18.06061237898499',
+            'radius': 100,
+            'count' : 10,
+            'tags': 'XXXXXX'
+        }
+        resp = requests.get(url, payload)
+        assert resp.json()['error'] == 'No results matched the tags provided'
+
+        payload = {
+            'lat': '99.33265972650577',
+            'lng': '-98.06061237898499',
+            'radius': 100,
+            'count' : 10,
+        }
+        resp = requests.get(url, payload)
+        assert resp.json()['error'] == "Values for lat and lng but be withing a range of 90 and -90"
+
+        payload = {
+            'lat': '59.33265972650577',
+            'lng': '18.06061237898499',
+            'radius': 10000,
+            'count' : 10,
+        }
+        resp = requests.get(url, payload)
+        assert resp.json()['error'] == "Your search can cover a radius range from 100 up to 2000 meters"
+
+        payload = {
+            'lat': '59.33265972650577',
+            'lng': '18.06061237898499',
+            'radius': 100,
+            'count' : -2,
+        }
+        resp = requests.get(url, payload)
+        assert resp.json()['error'] == "The number of products has to be between 10 and 50 products"
+
+        payload = {
+            'lat': '59.33265972650577',
+            'lng': '18.06061237898499',
+            'radius': 100,
+            'count' : 'HAHA',
+        }
+        resp = requests.get(url, payload)
+        assert resp.json()['error'] == "Incorrect input types, please make sure the following holds:- \
+radius: Integer, \
+lat:    Float, \
+lng:    Float, \
+radius: Integer, \
+tags:   String"
